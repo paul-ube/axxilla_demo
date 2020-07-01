576f32c1ba8b11daf99ccb03682316cdf3fba853
Paul <edward.paul.marzo@gmail.com>
2020-07-01 17:51:32 +0400

    - draft for weather app (inspired by OverDrop)

--- lib/ui/screens/studies/overdrop_clone/widgets/hourly_weather_section.dart
+++ lib/ui/screens/studies/overdrop_clone/widgets/hourly_weather_section.dart
@@ -21,7 +21,6 @@ class HourlyWeatherSection extends StatelessWidget {
   }
 }
 
-
 class _LineChart extends StatefulWidget {
   @override
   __LineChartState createState() => __LineChartState();
@@ -37,49 +36,65 @@ class __LineChartState extends State<_LineChart> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        AspectRatio(
-          aspectRatio: 1.70,
-          child: Container(
-            decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(
-                  Radius.circular(18),
-                ),
-                color: OverDropColors.background),
-            child: Padding(
-              padding: const EdgeInsets.only(right: 18.0, left: 12.0, top: 24, bottom: 12),
-              child: LineChart(
-                showAvg ? avgData() : mainData(),
-              ),
-            ),
-          ),
+    return Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(18),
         ),
-        SizedBox(
-          width: 60,
-          height: 34,
-          child: FlatButton(
-            onPressed: () {
-              setState(() {
-                showAvg = !showAvg;
-              });
-            },
-            child: Text(
-              'avg',
-              style: TextStyle(
-                  fontSize: 12, color: showAvg ? Colors.white.withOpacity(0.5) : Colors.white),
-            ),
-          ),
+      ),
+      child: SingleChildScrollView(
+        padding: EdgeInsets.zero,
+        scrollDirection: Axis.horizontal,
+        child: LineChart(
+          showAvg ? avgData() : mainData(),
         ),
-      ],
+      ),
     );
+//    return Stack(
+//      children: <Widget>[
+//        AspectRatio(
+//          aspectRatio: 1.70,
+//          child: Container(
+//            decoration: const BoxDecoration(
+//                borderRadius: BorderRadius.all(
+//                  Radius.circular(18),
+//                ),
+//                color: OverDropColors.background),
+//            child: SingleChildScrollView(
+//              scrollDirection: Axis.horizontal,
+//              child: LineChart(
+//                showAvg ? avgData() : mainData(),
+//              ),
+//            ),
+//          ),
+//        ),
+//        SizedBox(
+//          width: 60,
+//          height: 34,
+//          child: FlatButton(
+//            onPressed: () {
+//              setState(() {
+//                showAvg = !showAvg;
+//              });
+//            },
+//            child: Text(
+//              'avg',
+//              style: TextStyle(
+//                  fontSize: 12, color: showAvg ? Colors.white.withOpacity(0.5) : Colors.white),
+//            ),
+//          ),
+//        ),
+//      ],
+//    );
   }
 
   LineChartData mainData() {
     return LineChartData(
+
       gridData: FlGridData(
         show: true,
-        drawVerticalLine: true,
+        drawVerticalLine: false,
         drawHorizontalLine: false,
         getDrawingHorizontalLine: (value) {
           return FlLine(
@@ -94,26 +109,39 @@ class __LineChartState extends State<_LineChart> {
           );
         },
       ),
+
       titlesData: FlTitlesData(
         show: true,
+
         bottomTitles: SideTitles(
           showTitles: true,
-          reservedSize: 22,
-          textStyle:
-          const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 16),
+          reservedSize: 50,
+          textStyle: const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16),
           getTitles: (value) {
             switch (value.toInt()) {
+              case 1:
+                return '13';
               case 2:
-                return 'MAR';
+                return '14';
+              case 3:
+                return '15';
+              case 4:
+                return '16';
               case 5:
-                return 'JUN';
-              case 8:
-                return 'SEP';
+                return '17';
+              case 6:
+                return '18';
+              case 7:
+                return '19';
             }
             return '';
           },
           margin: 8,
         ),
+
         leftTitles: SideTitles(
           showTitles: false,
           textStyle: const TextStyle(
@@ -136,33 +164,35 @@ class __LineChartState extends State<_LineChart> {
           margin: 12,
         ),
       ),
-      borderData:
-      FlBorderData(show: true, border: Border.all(color: const Color(0xff37434d), width: 1)),
+      borderData: FlBorderData(
+          show: false,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
       minX: 0,
-      maxX: 11,
-      minY: 0,
-      maxY: 6,
+      maxX: 7,
+      minY: 30,
+      maxY: 40,
       lineBarsData: [
         LineChartBarData(
           spots: [
-            FlSpot(0, 3),
-            FlSpot(2.6, 2),
-            FlSpot(4.9, 5),
-            FlSpot(6.8, 3.1),
-            FlSpot(8, 4),
-            FlSpot(9.5, 3),
-            FlSpot(11, 4),
+            FlSpot(0, 35),
+            FlSpot(2.6, 36),
+            FlSpot(4.9, 36),
+            FlSpot(6.8, 36),
+            FlSpot(8, 35),
+            FlSpot(9.5, 35),
+            FlSpot(11, 34),
           ],
           isCurved: true,
-          colors: gradientColors,
-          barWidth: 5,
+          colors: [OverDropColors.blue],
+          barWidth: 3,
           isStrokeCapRound: true,
           dotData: FlDotData(
-            show: false,
+            show: true,
           ),
           belowBarData: BarAreaData(
             show: true,
-            colors: gradientColors.map((color) => color.withOpacity(0.3)).toList(),
+            colors:
+                gradientColors.map((color) => color.withOpacity(0.3)).toList(),
           ),
         ),
       ],
@@ -193,8 +223,10 @@ class __LineChartState extends State<_LineChart> {
         bottomTitles: SideTitles(
           showTitles: true,
           reservedSize: 22,
-          textStyle:
-          const TextStyle(color: Color(0xff68737d), fontWeight: FontWeight.bold, fontSize: 16),
+          textStyle: const TextStyle(
+              color: Color(0xff68737d),
+              fontWeight: FontWeight.bold,
+              fontSize: 16),
           getTitles: (value) {
             switch (value.toInt()) {
               case 2:
@@ -230,8 +262,9 @@ class __LineChartState extends State<_LineChart> {
           margin: 12,
         ),
       ),
-      borderData:
-      FlBorderData(show: true, border: Border.all(color: const Color(0xff37434d), width: 1)),
+      borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1)),
       minX: 0,
       maxX: 11,
       minY: 0,
@@ -249,8 +282,10 @@ class __LineChartState extends State<_LineChart> {
           ],
           isCurved: true,
           colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.2),
-            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.2),
+            ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                .lerp(0.2),
+            ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                .lerp(0.2),
           ],
           barWidth: 5,
           isStrokeCapRound: true,
@@ -258,12 +293,15 @@ class __LineChartState extends State<_LineChart> {
             show: false,
           ),
           belowBarData: BarAreaData(show: true, colors: [
-            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.2).withOpacity(0.1),
-            ColorTween(begin: gradientColors[0], end: gradientColors[1]).lerp(0.2).withOpacity(0.1),
+            ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                .lerp(0.2)
+                .withOpacity(0.1),
+            ColorTween(begin: gradientColors[0], end: gradientColors[1])
+                .lerp(0.2)
+                .withOpacity(0.1),
           ]),
         ),
       ],
     );
   }
 }
-
